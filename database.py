8a9232f4c64c2ca876fa08f5595ab1604a5e6b10
MAKHFIRAT2408 <makhfiratsaidova77@gmail.com>
2025-12-27 09:04:29 +0500

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLITE_DATABASE_URL = "sqlite:////data/food.db"
+SQLITE_DATABASE_URL = "sqlite:///data/food.db"
 
 engine = create_engine(SQLITE_DATABASE_URL, connect_args={"check_same_thread": False})
 
